13cf59a7783f1027dc616438d8ec2bc84bd3fe41
Vivek Rathore <rathorevivek9929@gmail.com>
2023-09-24 00:01:24 +0530

    feat : added all reducers ✨

---
+++ src/Redux/reducers/adminReducer.js
@@ -0,0 +1,123 @@
+import { createReducer } from '@reduxjs/toolkit';
+
+export const adminReducer = createReducer(
+  {},
+  {
+    getAdminStatsRequest: state => {
+      state.loading = true;
+    },
+    getAdminStatsSuccess: (state, action) => {
+      state.loading = false;
+      state.stats = action.payload.stats;
+      state.viewsCount = action.payload.viewsCount;
+      state.subscriptionsCount = action.payload.subscriptionsCount;
+      state.userCount = action.payload.userCount;
+      state.subscriptionPercentage = action.payload.subscriptionPercentage;
+      state.viewPercentage = action.payload.viewPercentage;
+      state.userPercentage = action.payload.userPercentage;
+      state.subscriptionProfit = action.payload.subscriptionProfit;
+      state.viewProfit = action.payload.viewProfit;
+      state.userProfit = action.payload.userProfit;
+      //         userCount(pin):1
+      // subscriptionsCount(pin):0
+      // viewsCount(pin):27
+      // userPercentage(pin):0
+      // subscriptionPercentage(pin):0
+      // viewPercentage(pin):2700
+      // userProfit(pin):true
+      // subscriptionProfit(pin):true
+      // viewProfit(pin):true
+    },
+    getAdminStatsFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    getAllUsersRequest: state => {
+      state.loading = true;
+    },
+    getAllUsersSuccess: (state, action) => {
+      state.loading = false;
+      state.users = action.payload;
+    },
+    getAllUsersFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateUserRoleRequest: state => {
+      state.loading = true;
+    },
+    updateUserRoleSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    updateUserRoleFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserRequest: state => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    deleteUserFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    createCourseRequest: state => {
+      state.loading = true;
+    },
+    createCourseSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    createCourseFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteCourseRequest: state => {
+      state.loading = true;
+    },
+    deleteCourseSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    deleteCourseFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    addLectureRequest: state => {
+      state.loading = true;
+    },
+    addLectureSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    addLectureFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    deleteLectureRequest: state => {
+      state.loading = true;
+    },
+    deleteLectureSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    deleteLectureFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    clearError: state => {
+      state.error = null;
+    },
+    clearMessage: state => {
+      state.message = null;
+    },
+  }
+);
